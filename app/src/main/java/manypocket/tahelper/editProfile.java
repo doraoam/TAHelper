4ef00ff63b8ef91e885a702fd443e1fbef8b424a
Supravee NILPRASERT <doraoam2005@gmail.com>
2015-03-31 16:26:56 +0700

    Change title of each page. Show alert dialog box when click "save" at editProfile.java.

--- app/src/main/java/manypocket/tahelper/editProfile.java
+++ app/src/main/java/manypocket/tahelper/editProfile.java
@@ -1,17 +1,42 @@
 package manypocket.tahelper;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class editProfile extends ActionBarActivity {
+
+    private EditText edtStudentID;
+    private EditText edtName;
+    private EditText edtSurname;
+    private EditText edtMajor;
+    private EditText edtFaculty;
+
+    private String strStudentID;
+    private String strName;
+    private String strSurname;
+    private String strMajor;
+    private String strFaculty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        initWidget();
+    }
+
+    private void initWidget() {
+        edtStudentID = (EditText)findViewById(R.id.editText_studentID);
+        edtName = (EditText)findViewById(R.id.editText_name);
+        edtSurname = (EditText)findViewById(R.id.editText_surname);
+        edtMajor = (EditText)findViewById(R.id.editText_major);
+        edtFaculty = (EditText)findViewById(R.id.editText_faculty);
     }
 
 
@@ -35,5 +60,44 @@ public class editProfile extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void clickSaveNewStudent(View view) {
+        strStudentID = edtStudentID.getText().toString();
+        strName = edtName.getText().toString();
+        strSurname = edtSurname.getText().toString();
+        strMajor = edtMajor.getText().toString();
+        strFaculty = edtFaculty.getText().toString();
+
+        showAddStudentDialog();
+    }
+
+    private void showAddStudentDialog() {
+        AlertDialog.Builder addStudentAlert = new AlertDialog.Builder(this);
+        addStudentAlert.setIcon(R.drawable.ic_action_warning);
+        addStudentAlert.setTitle("Add New Student to Database");
+        addStudentAlert.setMessage("StudentID : " + strStudentID + "\n" + "Name : "
+            + strName + "\n" + "Surname : " + strSurname + "\n" + "Major : "
+                + strMajor + "\n" + "Faculty : " + strFaculty);
+
+        addStudentAlert.setPositiveButton("OK",
+                new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        addStudentAlert.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        addStudentAlert.show();
     }
 }
